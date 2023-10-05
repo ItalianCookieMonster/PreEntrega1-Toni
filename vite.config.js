436be4cdca0dca4e6b45d9e8fafe0c29b7ff75de
Valentina Toni <vale.toni93@gmail.com>
2023-10-05 16:16:25 +0100

    uninstalled sass

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import ViteSass from 'vite-plugin-sass';
+// import ViteSass from 'vite-plugin-sass';
 import react from '@vitejs/plugin-react'
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteSass()],
+  plugins: [react()],
 
 })
