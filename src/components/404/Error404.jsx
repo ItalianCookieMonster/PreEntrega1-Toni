628ce8a3ea059b92a1da3b5eac5d4de42b689ecd
Valentina Toni <vale.toni93@gmail.com>
2023-10-06 19:00:43 +0100

    solved 404

--- src/components/404/Error404.jsx
+++ src/components/404/Error404.jsx
@@ -13,8 +13,8 @@ const Error404 = () => {
   return (
     <Container className='d-flex flex-column justify-content-center align-items-center p-4'>
       <h2>Ooops, it looks like you're in the wrong place.</h2>
-      <img width='400px' height='400px' className='mt-3' style={{ borderRadius: '50%' }} src='/assets/img/404-error.jpg' alt='under construction' />
-      <Link to='/'><MyButton>Go to Homepage</MyButton></Link>
+  <img width='400px' height='auto' className='mt-3' style={{ borderRadius: '50%' }} src='/assets/img/404-error.jpg' alt='under construction' />
+      <Link to='/'><MyButton text='Go to Hompage' /> </Link>
     </Container>
   )
 }
